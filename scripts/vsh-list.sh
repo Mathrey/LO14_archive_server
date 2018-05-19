7498b83c4db5dbd6742f8f771e5511fe5306b0ed
paul_robineau <paul.robineau@utt.fr>
2018-05-19 21:11:28 +0200

    Fonction vsh-list réalisée si tout va bien. Mofidications à faire lorsque le fichier server sera avancé et l'appellera comme fonction (HERE => $2 + suppression du lancement de l'appel présent dans vsh-list lui-même)

--- scripts/vsh-list.sh
+++ scripts/vsh-list.sh
@@ -1,15 +1,20 @@
 #!bin/bash
 
+# Les fonctions sont appelées par le script "server"
+# Le dossier "script" et le dossier "archives" sont au même niveau d'arborescence
+# Il faut donc régulièrement avoir recours à cd ../archives pour intéragir avec son contenu 
+
 function vsh_list() {
 
-	case $input in
-
-		list && \w+ && \d+ ) echo "Bonne syntaxe"
-			;;
-
-		*) echo "usage : list [NOM_SERVEUR] [PORT]"
-			;;
-
-	esac 
+	nb_lignes=$(ls -t ../archives/ | wc -l) # le nombre de lignes du ls du dossier archive donne le nombre d'archives
+	echo ""
+	echo "$nb_lignes archives are currently on HERE" # le HERE sera remplacé par $2 lorsque le fichier sera appelé par server
+	echo ""
+	echo "The folowing list is sorted by time modified (most recently modified first) :"
+	echo ""
+	ls -1t ../archives/
+	echo ""
 
 }
+
+vsh_list # indispensable actuellement pour tester la fonction (vu qu'elle n'est pas encore appelée par server)
