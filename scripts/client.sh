ca1b101f807ee18b6edf358e30a2cbef7126474e
Mathieu Guéry <mathguery@gmail.com>
2018-05-19 20:17:54 +0200

    modified client.sh

--- scripts/client.sh
+++ scripts/client.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+
+echo "server.sh should be running at this point"
+echo "Reminder :"
+echo "vsh -list [SERVER_NAME] [PORT]"
+echo "vsh -browse [SERVER_NAME] [PORT] [ARCHIVE_NAME]"
+echo "vsh -extract [SERVER_NAME] [PORT] [ARCHIVE_NAME]"
+echo "For local server name should be localhost"
+
+read input?"Waiting for vsh input..."
+
+local $MODE = cut -f input 1
+
+if (test $# -eq 1) && (echo $1 | grep -E -q "^[0-9]+$")
+then
+	#nc localhost $1
+	echo "Client is running"
+else
+	usage
+fi
+
+
