622f1828f7b641013040204b0091346df2e04b59
Mathieu Guéry <mathguery@gmail.com>
2018-05-19 18:20:34 +0200

    server modified, testing conflicts gestion

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+function usage {
+	echo "Usage : $(basename $0) PORT_NUMBER"
+	exit -1
+}
+
+if (test $# -eq 1) && (echo $1 | grep -E -q "^[0-9]+$")
+then
+	#nc -lp $1 -e vsh
+	echo "it works !"
+else
+	usage
+fi
