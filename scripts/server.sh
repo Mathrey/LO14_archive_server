7d8f07fbb31107718835730c2c1cdf75dee04116
Mathieu Guéry <mathguery@gmail.com>
2018-05-20 17:44:38 +0200

    Server and client functionnal, at least for vsh-list

--- scripts/server.sh
+++ scripts/server.sh
@@ -6,62 +6,60 @@ function usage {
 	exit -1
 }
 
-# Création du tube nommé pour les interactions entre serveur et client
+# Création du dossier tmp pour les interactions entre serveur et client
 if [ ! -e "./tmp" ]
 then
 	mkdir ./tmp
-	echo "FIFO created"
+	echo "tmp/ created"
 fi
 
-FIFO="./tmp/FIFO"
-
 # Fonction de nettoyage du dossier
-function nettoyage() { rm -f "$FIFO"; }
+function nettoyage() { rm -f tmp/tube ; rm -f tmp/request.txt ; }
 # Lance nettoyage lorsque que le programme est quitté
 trap nettoyage EXIT
 
-# Créer le tube nommé FIFO si le fichier FIFO n'existe pas
-[ -e "$FIFO" ] || mkfifo "$FIFO"
-
-# Fonction qui renvoie aux fonctions de vsh
-# Ne marche pas, à revoir
-#function readinput() {
-#	local cmd args
-#	while true; do
-#		read cmd args || exit -1
-#		fun="commande-$cmd"
-#		if [ "$(type -t $fun)" = "function" ]; then
-#			$fun $args
-#		else
-#			echo "Unknown command"
-#		fi
-#	done
-#}
+# Pour chaque cas :
+# On crée le tube nommé s'il n'existe pas avec mkfifo
+# On remplit le tube avec l'output de la fonction vsh correspondante et on envoie le contenu du tube sur la connexion
+# On supprime le tube
 
 function vsh() {
 	case $1 in
+
 		"-list" )
-		echo "mode list activated"
-		bash ./vsh-list.sh args
+		echo "list mode requested @ "$(date +%T)
+		[ -e tmp/tube ] || mkfifo tmp/tube
+		nc -lp $3 < tmp/tube | bash ./vsh-list.sh args currentDirectory > tmp/tube
+		echo "request sent @ "$(date +%T)
+		rm tmp/tube
 		;;
 
 		"-extract" )
-		echo "mode exctract activated"
-		#bash vsh-extract.sh args
+		echo "mode extract requested (but not yet functionnal)"
+		[ -e tmp/tube ] || mkfifo tmp/tube
+#		nc -lp $3 < tmp/tube | bash ./vsh-extract.sh args > tmp/tube
+		nc -lp $3 < tmp/tube | echo "extract mode not yet implemented" > tmp/tube
+		rm tmp/tube
 		;;
 
 		"-browse" )
-		echo "mode browse activated"
-		#bash vsh-browse.sh args
+		echo "mode browse requested (but not yet functionnal)"
+		[ -e tmp/tube ] || mkfifo tmp/tube
+#		nc -lp $3 < tmp/tube | bash ./vsh-browse.sh args > tmp/tube
+		nc -lp $3 < tmp/tube | echo "browse mode not yet implemented" > tmp/tube
+		rm tmp/tube
 		;;
 
 		* )
-		echo "unknown mode"
+		echo "wrong request"
+		[ -e tmp/tube ] || mkfifo tmp/tube
+		nc -lp $3 < tmp/tube | echo "this mode does not exist"
+		rm tmp/tube
 		;;
 	esac
 }
 
-# Vérifie que le 1er argument est un nombre
+# Vérifie que le seul argument au lancement est un nombre
 if (test $# -eq 1) && (echo $1 | grep -E -q "^[0-9]+$")
 then
 	echo -e "\nit works !\n"
@@ -69,10 +67,22 @@ else
 	usage
 fi
 
-# Partie serveur
+# Le serveur tourne
 while true; do
-	echo "waiting input"
-# Écrit la réception dans le dossier FIFO
-#	nc -lp $1 > "$FIFO"
+	echo "waiting input from client"
+# Écrit la réception sur le port $1 dans le fichier input.txt
+	nc -lp $1 > tmp/request.txt
 	echo "input done !"
+# Associe le fichier input récupéré dans la variable input
+#	input=$(cat tmp/request.txt)
+	request=$(sed -n '1p' tmp/request.txt)
+	currentDirectory=$(sed -n '2p' tmp/request.txt)
+# Lit input et convertit en commande/arguments
+	read cmd args <<< $(echo $request)
+# Lance vsh avec les arguments si reconnu
+	if [ "$(type -t $cmd)" = "function" ]; then
+		$cmd $args $currentDirectory
+	else
+		echo "command unknown"
+	fi
 done
