a02022dd17a3e16870211b0f4c3ccb7a50af3597
Mathieu Guéry <mathguery@gmail.com>
2018-05-26 21:51:11 +0200

    Added commentaries to -browse cd

--- scripts/vsh-browse.sh
+++ scripts/vsh-browse.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-#if $4 is empty then echo "pas d'archive entrée" exit 1
-#si le temps le permet :
-#gérer cd .. ; cd ../../../../ ... ;
-
+# Ferme le client si l'utilisateur a oublié de mettre l'archive
 if [ -z $4 ]
 then
 	echo "No archive given"
@@ -11,13 +8,16 @@ then
 	exit 1
 fi
 
+# Variables locales
 currentArchive=$(cat ../archives/$4)
 currentDirectory=/
 root=$(echo "$currentArchive" | head -n 3 | tail -n 1 | cut -d" " -f2 | sed 's/\/$//') #Récupère la racine de l'archive
 
 echo -n "vsh:> "
+# Tant que l'utilisateur ne quitte pas le programme
 while read input
 do
+	# On change les arguments pour les gérer avec $1 $2 etc.
 	set -- $(echo $input)
 	case $1 in
 
@@ -35,7 +35,7 @@ do
 		"cd" )
 		path=$2
 
-		# On retire le "/" à la fin du chemin
+		# On retire le possible "/" à la fin d'un chemin (sauf si cd /)
 		if (echo "$path" | egrep -q "/$")
 		then
 			path=$(echo "$path" | sed 's/.$//')
@@ -45,7 +45,9 @@ do
 		if (echo "$path" | egrep -q "^/")
 		# Chemin absolu
 		then
-			# Note : il y a un bug lorsque l'on rajoute "$" à la fin de la régex, tous les dossiers sont trouvés sauf /A/A2 et /A/A3
+			# On vérifie que le chemin est présent dans l'archive
+			# BUG : il y a un bug lorsque l'on rajoute "$" à la fin de la régex, tous les dossiers sont trouvés sauf /A/A2 et /A/A3, le grep récupère donc tous les résultats où le chemin apparaît (ce qui ne gêne pas puisqu'on veut juste vérifier qu'il existe)
+			# FIX : enlever le $ à la fin de la régex
 			if (echo "$currentArchive" | egrep -q "^directory $root$path")
 			then
 				currentDirectory=$(echo "$path")
@@ -54,33 +56,45 @@ do
 			fi
 		# Chemin relatif
 		else
+			# Si cd
 			if [ -z "$path" ]
 			then
 				currentDirectory=/
+			# Si cd ..
 			elif [ "$path" = ".." ]
 			then
+				# Si on est déjà à la racine
 				if [ "$currentDirectory" = "/" ]
 				then
 					currentDirectory=/
 				else
 					currentDirectory=$(echo "$currentDirectory" | egrep -o ".*/")
+					# Si on n'est pas revenu à la racine on retire le "/" de fin
 					if [ "$currentDirectory" != "/" ]
 					then
 						currentDirectory=$(echo "$currentDirectory" | sed 's/\/$//')
 					fi
 				fi
+			# Si cd Dossier/Dossier1 etc.
 			else
+				# À cause des "/", on traite différemment la ligne de texte selon que l'on soit sur la racine ou non
+				# Si on est dans la racine
 				if [ "$currentDirectory" = "/" ]
 				then
+					# Si le chemin est présent dans l'archive
 					if (echo "$currentArchive" | egrep -q "$root$currentDirectory$path")
 					then
+						# Pas besoin de rajouter un "/" entre l'origine et la destination
 						currentDirectory=$(echo "$currentDirectory$path")
 					else
 						echo "No directory found"
 					fi
+				# Si on est ailleurs qu'à la racine
 				else
+					# Si le chemin est présent dans l'archive
 					if (echo "$currentArchive" | egrep -q "$root$currentDirectory/$path")
 					then
+						# On ajoute le "/" entre l'origine et la destination
 						currentDirectory=$(echo "$currentDirectory/$path")
 					else
 						echo "No directory found"
