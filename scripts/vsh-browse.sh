1822bd83dfc509e61ca750cd54947abf442a2ff5
Mathieu Guéry <mathguery@gmail.com>
2018-05-26 18:51:58 +0200

    -browse cd operationnal, except for cd .. and ../.. etc.

--- scripts/vsh-browse.sh
+++ scripts/vsh-browse.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+#if $4 is empty then echo "pas d'archive entrée" exit 1
+#si le temps le permet :
+#gérer cd .. ; cd ../../../../ ... ;
+
+if [ -z $4 ]
+then
+	echo "No archive given"
+	echo "Usage : vsh -browse [SERVER_NAME] [PORT] [ARCHIVE_NAME]"
+	exit 1
+fi
+
 currentArchive=$(cat ../archives/$4)
 currentDirectory=/
+root=$(echo "$currentArchive" | head -n 3 | tail -n 1 | cut -d" " -f2 | sed 's/\/$//') #Récupère la racine de l'archive
 
-echo "vsh:> "
+echo -n "vsh:> "
 while read input
 do
 	set -- $(echo $input)
-	echo $2
-	echo $#
 	case $1 in
 		"pwd" )
-		echo $currentDirectory
+		echo "$currentDirectory"
 		;;
 
 		"ls" )
@@ -19,7 +29,50 @@ do
 		;;
 
 		"cd" )
-		echo "commande cd"
+		path=$2
+
+		# On retire le "/" à la fin du chemin
+		if (echo "$path" | egrep -q "/$")
+		then
+			path=$(echo "$path" | sed 's/.$//')
+		fi
+
+		# On teste si chemin absolu ou relatif
+		if (echo "$path" | egrep -q "^/")
+		# Chemin absolu
+		then
+			# Note : il y a un bug lorsque l'on rajoute "$" à la fin de la régex, tous les dossiers sont trouvés sauf /A/A2 et /A/A3
+			if (echo "$currentArchive" | egrep -q "^directory $root$path")
+			then
+				currentDirectory=$(echo "$path")
+			else
+				echo "$path"
+				echo "No directory found"
+			fi
+		# Chemin relatif
+		else
+			if [ -z "$path" ]
+			then
+				currentDirectory=/
+			else
+				if [ "$currentDirectory" = "/" ]
+				then
+					if (echo "$currentArchive" | egrep -q "$root$currentDirectory$path")
+					then
+						currentDirectory=$(echo "$currentDirectory$path")
+					else
+						echo "No directory found"
+					fi
+				else
+					if (echo "$currentArchive" | egrep -q "$root$currentDirectory/$path")
+					then
+						currentDirectory=$(echo "$currentDirectory/$path")
+					else
+						echo "No directory found"
+					fi
+				fi
+			fi
+		fi
 		;;
 
 		"cat" )
@@ -33,5 +86,5 @@ do
 		* )
 		echo "unknown command"
 	esac
-	echo "vsh:> "
+	echo -n "vsh:> "
 done
